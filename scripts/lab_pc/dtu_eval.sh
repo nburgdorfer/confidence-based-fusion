e766794a620b221426d42881ab50bdd7f32d39ef
nburgdorfer <nburgdorfer@gmail.com>
2022-01-05 15:47:12 -0500

    Modifying scripts

--- scripts/lab_pc/dtu_eval.sh
+++ scripts/lab_pc/dtu_eval.sh
@@ -16,4 +16,4 @@ OPTIONS:
 DATA_DIR=/media/nate/Data/MVSNet/dtu/testing/
 EVAL_DIR=/media/nate/Data/Evaluation/dtu/
 
-./dtu_eval.sh -c ${EVAL_DIR}matlab_code/ -d ${DATA_DIR} -p ${EVAL_DIR}mvs_data/Points/ -r ${EVAL_DIR}mvs_data/Results
+./dtu_eval.sh -c ${EVAL_DIR}matlab_code/ -d ${DATA_DIR} -p ${EVAL_DIR}mvs_data/Points/fusion/ -r ${EVAL_DIR}mvs_data/Results
